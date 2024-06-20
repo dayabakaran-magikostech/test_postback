5b518e8aa2a415803c250488ade1b2414491a79c
dayabakaran-magikostech <karan.dayaba@magikostech.com>
2024-06-20 14:12:51 +0530

    Modified code

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 })
 
 app.post('/order-postback', (req, res) => {
 	const originalBody = { ...req.body };
-	console.log(originalBody);
+	console.log(req);
+	// console.log(originalBody);
 	res.status(200).send('OK');
 
 })
