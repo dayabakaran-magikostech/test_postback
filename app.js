7abaec301c2715cf03b3367a6bd66a6b49410a6c
dayabakaran-magikostech <karan.dayaba@magikostech.com>
2024-06-20 13:44:28 +0530

    seccond commit

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 9000
 
-app.get('/order-postback', (req, res) => {
-	res.send('Hello World!')
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+})
+
+app.post('/order-postback', (req, res) => {
+	const originalBody = { ...req.body };
+	console.log(originalBody);
+	res.status(200).send('OK');
+
 })
 
 app.listen(port, () => {
